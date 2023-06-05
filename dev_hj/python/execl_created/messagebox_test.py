cd234a8359403b466b239578b4892b3f59a14ca5
HeeJeong <imheejeong@westworld.co.kr>
2023-06-05 13:11:10 +0900

    messagebox_test

---
+++ dev_hj/python/execl_created/messagebox_test.py
@@ -0,0 +1,38 @@
+
+from PySide2.QtWidgets import *
+from PySide2 import QtGui, QtCore
+
+
+import sys
+app = QApplication()
+
+class Dialog(QDialog):
+    def __init__(self, parent=None):
+        super(Dialog, self).__init__(parent)
+
+        self.resize(300, 100)
+        self.setLayout(QVBoxLayout())
+
+        button = QPushButton('Submit')
+        button.clicked.connect(self.onclick)
+        self.layout().addWidget(button)
+
+    def onclick(self):
+        self.close()
+        message = "<font size = 5 color = gray > Rich Html Title </font> <br/><br/>The clickable link <a href='http://www.google.com'>Google.</a> The lower and upper case text."
+        messagebox = QMessageBox(QMessageBox.Warning, "Successful", message, parent=self)
+        messagebox.addButton("ResetRole Left Most", QMessageBox.ResetRole)
+        messagebox.addButton("ApplyRole Left", QMessageBox.ApplyRole)
+        messagebox.addButton("RejectRole Right", QMessageBox.RejectRole)
+        messagebox.exec_()
+        # print('exe: %s  clickedButton: %s'%(exe, messagebox.clickedButton())
+
+
+
+dialog = Dialog()
+dialog.show()
+app.exec_()
+
+./configure
+make
+sudo make install
